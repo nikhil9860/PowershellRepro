bf9707cbf1f1a39b3ddbccd8b3004da0de5f879e
Nikhil gaikwad <nikhil9860@gmail.com>
2023-05-19 08:18:04 +0530

    Added more changes

--- VSPStesting.ps1
+++ VSPStesting.ps1
@@ -6,8 +6,8 @@ $token = $open_id.token_endpoint
 
 $body = @{
 
-    client_id = ""
-    client_secret = ""
+    client_id = "###"
+    client_secret = "###"
     redirect_uri = "https://localhost"
     grant_type = "client_credentials"
     resource = "https://graph.microsoft.com"
